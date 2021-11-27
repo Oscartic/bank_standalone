8637f448a2bc4b7380da0f60f8c9157d0d2ba6a2
Oscar Ballesteros <oscartic@gmail.com>
2021-11-27 16:10:28 -0300

    add user to context app

--- alldata.js
+++ alldata.js
@@ -1,11 +1,35 @@
 const AllData = () => {
+  
+  const ctx = React.useContext(UserContext);
+
   return (
     <>
     <div className="container-fluid">
       <div className="row">
         <div className="col">
           <h1>All Data</h1>
-          { JSON.stringify(ctx) }
+          <table class="table">
+            <thead class="table-dark">
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Password</th>
+              <th scope="col">Balance</th>
+            </tr>
+            </thead>
+            <tbody>
+              {
+                ctx.users.map(u => (
+                  <tr>
+                    <th>{u.name}</th>
+                    <th>{u.email}</th>
+                    <th><code>{u.password}</code></th>
+                    <th>{u.balance}</th>
+                  </tr>
+                ))
+              }
+            </tbody>
+        </table>
         </div>
       </div>
     </div>
