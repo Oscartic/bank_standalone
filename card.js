2610fdcbbeab79e67196511946f18c133c998bc4
Oscar Ballesteros <oscartic@gmail.com>
2021-11-27 15:32:25 -0300

    style card

--- card.js
+++ card.js
@@ -1,11 +1,23 @@
-const Card = () => {
+const Card = ({header, title, text, body, status, bgcolor, txtcolor}) => {
+
+    const classes = () => {
+        const bg = bgcolor ? ` bg-${bgcolor}` : ''; 
+        const txt = txtcolor ? ` text-${txtcolor}` : ' text-white';
+        return `card mb-3 ${bg} ${txt}`;   
+    }
+
     return (
         <div className="card" style={{width: "18rem"}}>
-            <img src="./bank.png" className="card-img-top" alt="..." />
+            <div className="card-header">
+                {header}
+            </div>
             <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">{title && (<strong className="card-title">{title}</strong>)}</h5>
+                <p className="card-text">
+                    {text && (<p className="card-text">{text}</p>)}
+                    {body}
+                    {status && (<div className="createStatus">{status}</div>)}
+                </p>
             </div>
         </div>
     )
--- home.js
+++ home.js
@@ -7,7 +7,14 @@ const Home= () => {
       <div className="row">
         <div className="col">
           <h1>Home</h1>
-          <Card />
+          <Card 
+            bgcolor="secondary"
+            txtcolor="white"
+            header="BadBank landing Page"
+            title="Welcome to the Bank"
+            text="You can use this bank"
+            body={(<img src="https://ik.imagekit.io/orb/bank_bl8qVd2IE.png?updatedAt=1637803496240" className="img-fluid" alt="responsive image" />)}
+          />
         </div>
       </div>
     </div>
